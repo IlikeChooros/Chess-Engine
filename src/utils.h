1ed774d245a58df57631cb846b2eb898f5d181b7
IlikeChooros <pawel.smolnicki@gmail.com>
2024-07-17 20:49:21 +0200

    chore: Fix printf statement in utils.h

--- src/utils.h
+++ src/utils.h
@@ -17,7 +17,7 @@ inline std::string square_to_str(int square){
 #   define dbitboard(__bitboard64) do { \
     for (int i = 0; i < 8; i++) { \
         for (int j = 0; j < 8; j++) { \
-            printf("%*lu", 2, ); \
+            printf("%*lu", 2, (__bitboard64 >> (i * 8 + j)) & 1); \
         } \
         printf("\n"); \
     } \
